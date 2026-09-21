fa3cc8f9f34a491c737c1564dc3de66fb02f9cd7
Sneha037 <mukherjeesneha037@gmail.com>
2026-09-22 04:43:28 +0530

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isPalindrome(String s) 
+    {
+        s = s.trim();
+        s = s.toLowerCase();
+
+        int n = s.length();
+
+        int l = 0;
+        int r = n-1;
+
+        while(l<=r)
+        {
+            char ch1 = s.charAt(l);
+            char ch2 = s.charAt(r);
+
+            if(!((ch1 >= 'a' && ch1 <= 'z') || (ch1 >= '0' && ch1 <= '9')))
+            {
+                l++;
+                continue;
+            }
+            else if(!((ch2 >= 'a' && ch2 <= 'z') || (ch2 >= '0' && ch2 <= '9')))
+            {
+                r--;
+                continue;
+            }
+
+            if(ch1 != ch2)
+            {
+                return false;
+            }
+
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
