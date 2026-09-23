251e32e71ea9431d3c9789f6828ddc673e97f180
Sneha037 <mukherjeesneha037@gmail.com>
2026-09-23 05:59:21 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) 
+    {
+        int n = prices.length;
+
+        int min = prices[0];
+
+        int ans = 0;
+
+        for(int i=1; i<n; i++)
+        {
+            min = Math.min(min, prices[i]);
+
+            ans = Math.max(ans, prices[i] - min);
+        }        
+
+        return ans;
+    }
+}
